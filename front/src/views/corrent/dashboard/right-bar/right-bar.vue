8ee51e45b43a2683b266674de7a3303add18abdb
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-03 22:18:00 +0900

    feat: right에 todolist 컴포넌트 추가

---
+++ front/src/views/corrent/dashboard/right-bar/right-bar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="right-bar">
+        <Section>
+            section1
+        </Section>
+
+        <Section>
+            <TodoList/>
+        </Section>
+    </div>
+</template>
+
+<script>
+import Section from '@/views/common/section'
+import TodoList from '../todo-list/todo-list'
+
+export default {
+    name: 'rightBar',
+
+    components: {
+        Section,
+        TodoList,
+    }
+}
+</script>
+
+<style>
+    @import url('./right-bar.css');
+</style>
